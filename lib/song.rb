5ccac02b7166a29543e4d6ec2c471c80bd3809d4
John Zonneveld <jjczon@gmail.com>
2020-04-02 17:37:05 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,24 @@
+class Song
+    attr_accessor :name, :artist, :genre
+    @@count = 0
+    
+    def initialize(name, artist, genre)
+        @@count += 1
+    end
+    
+    def artists
+
+    end
+    def count
+
+    end
+    def genres
+
+    end
+    def genres_count
+
+    end
+    def artist_count
+
+    end
+end
